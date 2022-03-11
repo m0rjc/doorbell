ae142accd83f1671b8db852fe530903f11bcedc6
Richard Corfield <richard.corfield@gmail.com>
2022-03-11 07:27:40 +0000

    Allow config over a connected WiFi network when in config mode

--- main/wifi.c
+++ main/wifi.c
@@ -81,7 +81,7 @@ void wifi_keepalive_task(void *pvParameter) {
 
 void wifi_init_sta(void)
 {
-    int hasStationConfig = strlen(m0rjc_config.ssid) > 0 && DIP_IS_MODE_RUN_WIFI;
+    int hasStationConfig = strlen(m0rjc_config.ssid) > 0;
     int allowAP = !hasStationConfig || DIP_IS_MODE_CONFIG;
 
     ESP_ERROR_CHECK(esp_netif_init());
